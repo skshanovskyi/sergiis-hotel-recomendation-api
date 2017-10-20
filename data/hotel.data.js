4ace319768bbb94d6555b45f8e35b9ff635a3916
sks <sks@ciklum.com>
2017-10-20 16:46:18 +0300

    randomizer added

--- data/hotel.data.js
+++ data/hotel.data.js
@@ -2,6 +2,22 @@
 
 module.exports = [ {
   name: 'Hotel1',
-  description: 'Description',
-  img: 'http://dreamatico.com/data_images/hotels/hotels-4.jpg'
+  description: 'Description1 description1 description1 description1 description1 description1 description1',
+  img: 'http://www.ahstatic.com/photos/5413_ho_00_p_1024x768.jpg'
+}, {
+  name: 'Hotel2',
+  description: 'Description2 description2 description2 description2 description2 description2 description2',
+  img: 'http://pix10.agoda.net/hotelImages/153/1538/1538_14021600400018351674.jpg?s=1024x768'
+}, {
+  name: 'Hotel3',
+  description: 'Description3 description3 description3 description3 description3 description3 description3',
+  img: 'http://pix10.agoda.net/hotelImages/283/2836/2836_16033015560041176025.jpg?s=1024x768'
+}, {
+  name: 'Hotel4',
+  description: 'Description4 description4 description4 description4 description4 description4 description4',
+  img: 'http://pix10.agoda.net/hotelImages/843/84307/84307_14070916270020205875.jpg?s=1024x768'
+}, {
+  name: 'Hotel5',
+  description: 'Description5 description5 description5 description5 description5 description5 description5',
+  img: 'http://www.ahstatic.com/photos/0937_ho_00_p_1024x768.jpg'
 } ];
--- services/hotel.service.js
+++ services/hotel.service.js
@@ -2,4 +2,19 @@
 
 const hotelsList = require('../data/hotel.data');
 
-exports.getList = () => Promise.resolve(hotelsList);
+exports.getList = () => {
+  let result = [undefined, undefined];
+  return Promise.resolve(result.map(() => {
+    return getHotelByIndex(getRandomHotelIndex());
+  }));
+};
+
+function getRandomHotelIndex() {
+  let max = hotelsList.length - 1;
+  let min = 0;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getHotelByIndex(index) {
+  return hotelsList[index];
+}
